74d1bb1fe2a81150a4c9f1f5033cff789c3206de
Daniel <d972any@proton.me>
2024-08-10 11:30:27 +0200

    Scraping fixed

--- src/embeds/embed.js
+++ src/embeds/embed.js
@@ -5,7 +5,7 @@ module.exports = {
         return new EmbedBuilder()
             .setColor('#fcba03')
             .setTitle(title || 'URL Detected')
-            .setDescription(`A valid page URL has been detected.\nPrice: ${price ? "¥" + price : 'No available'}`)
+            .setDescription(`A valid page URL has been detected.\nPrice: ${price ? price : 'No available'}`)
             .addFields({ name: 'URL', value: url })
             .setTimestamp()
             .setFooter({ text: 'Reps Bot', iconURL: 'https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1095249842.jpg' });
--- src/scraping/getPageData.js
+++ src/scraping/getPageData.js
@@ -1,67 +1,53 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
-const fs = require('fs');
 
 const getPageData = async (url) => {
     let browser;
     try {
-        console.log('Launching the browser...');
-        browser = await puppeteer.launch({ headless: true });
+        browser = await puppeteer.launch({ headless: true, defaultViewport: null });
         const page = await browser.newPage();
 
-        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+        await page.setViewport({ width: 1200, height: 800 });
 
-        console.log(`Navigating to URL: ${url}`);
-        await page.goto(url, { waitUntil: 'networkidle2' });
+        await page.goto('https://basetao.com', { waitUntil: 'networkidle2' });
 
-        console.log('Taking screenshot of the page...');
-        await page.screenshot({ path: 'screenshot.png' });
+        await page.waitForSelector('input[name="taobao-url"]', { visible: true });
+        await page.type('input[name="taobao-url"]', url);
+
+        const submitButtonSelector = 'button[type="submit"]';
+        await page.waitForSelector(submitButtonSelector, { visible: true, timeout: 60000 });
+        const submitButton = await page.$(submitButtonSelector);
+        if (submitButton) {
+            await submitButton.click();
+        } else {
+            throw new Error('Submit button not found.');
+        }
+
+        await page.waitForFunction(
+            () => document.querySelector('.py-0') !== null,
+            { timeout: 30000 } // Ajusta el timeout si es necesario
+        );
 
         const content = await page.content();
-        console.log('Obtained HTML content.');
-
         const $ = cheerio.load(content);
+
 
         let title = '', price = '';
 
-        if (url.includes('taobao.com')) {
-            console.log('Extracting data from TaoBao...');
-            title = $('#item-title').text().trim();
-            price = $('#price').text().trim();
-        } else if (url.includes('weidian.com')) {
-            console.log('Extracting data from Weidian...');
-            title = $('.item-title-content').text().trim();
-            price = $('.cur-price.wd-theme__price').text().trim();
-        } else if (url.includes('1688.com')) {
-            console.log('Extracting data from 1688...');
-            title = $('.title-text').text().trim();
-            price = $('.price-text').text().trim();
+        const titleSelector = '#check_titel';
+        const priceSelector = 'span.text-orange.me-3.fw-bold.fs-3';
 
-            if (!title && !price) {
-                console.log('It looks like there is a CAPTCHA or the data was not extracted correctly.');
-            } else {
-                console.log(`Extracted title: ${title}`);
-                console.log(`Extracted price: ${price}`);
-            }
-        } else {
-            throw new Error('URL not supported for scraping.');
-        }
+        title = $(titleSelector).text().trim();
+        price = $(priceSelector).text().trim();
 
         return { title, price };
 
     } catch (error) {
         console.error('Error when scraping:', error);
-
-        if (browser) {
-            console.log('Taking screenshot of the error...');
-            await page.screenshot({ path: 'error.png' });
-        }
-
         return { title: 'Error', price: 'Error' };
     } finally {
         if (browser) {
             await browser.close();
-            console.log('Browser closed.');
         }
     }
 };
